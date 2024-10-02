60633e9cec0c625f30e1d96916759a6746a271da
Oliver Maus <oliver.maus@eclipso.de>
2024-10-02 16:59:49 +0200

    Add Invenio unit tests

--- tests/invenio@3.0.0.fs
+++ tests/invenio@3.0.0.fs
@@ -6,96 +6,43 @@ open Xunit
 open ARCTokenization
 open ControlledVocabulary
 open System
+open Fake.DotNet
+open TestUtils
+open ARCExpect
+open System.Text.Json
+open ValidationPackages.Tests
 
 
-let arcDir = Path.Combine(__SOURCE_DIRECTORY__, "fixtures", "ArcPrototype")
+//let arcDir = Path.Combine(__SOURCE_DIRECTORY__, "fixtures", "ArcPrototype")
 
-let absoluteDirectoryPaths = FileSystem.parseARCFileSystem arcDir
+//let absoluteDirectoryPaths = FileSystem.parseARCFileSystem arcDir
 
-let investigationMetadata = 
-    absoluteDirectoryPaths
-    |> Investigation.parseMetadataSheetsFromTokens() arcDir 
-    |> List.concat 
+//let investigationMetadata = 
+//    absoluteDirectoryPaths
+//    |> Investigation.parseMetadataSheetsFromTokens() arcDir 
+//    |> List.concat 
 
-// Investigation has title
-let expectedTitleCvPExists =
-    investigationMetadata 
-    |> List.exists (
-        fun ip -> 
-            Param.getCvAccession ip = "INVMSO:00000009" && 
-            Param.getValueAsString ip |> String.IsNullOrWhiteSpace |> not && 
-            match Param.getParamValue ip with | CvValue x -> x.Accession = "AGMO:00000001" | _ -> false
-    )
+//// Investigation has title
+//let expectedTitleCvPExists =
+//    investigationMetadata 
+//    |> List.exists (
+//        fun ip -> 
+//            Param.getCvAccession ip = "INVMSO:00000009" && 
+//            Param.getValueAsString ip |> String.IsNullOrWhiteSpace |> not && 
+//            match Param.getParamValue ip with | CvValue x -> x.Accession = "AGMO:00000001" | _ -> false
+//    )
 
-Assert.True expectedTitleCvPExists
+//Assert.True expectedTitleCvPExists
 
 
-// Validation Cases:
-let cases = 
-    testList INVMSO.``Investigation Metadata``.INVESTIGATION.key.Name [
-        //// Investigation has title
-        //ARCExpect.validationCase (TestID.Name INVMSO.``Investigation Metadata``.INVESTIGATION.``Investigation Title``.Name) {
-        //    investigationMetadata
-        //    |> Validate.ParamCollection.ContainsNonKeyParamWithTerm
-        //        INVMSO.``Investigation Metadata``.INVESTIGATION.``Investigation Title``
-        //}
-        // Investigation has description
-        ARCExpect.validationCase (TestID.Name INVMSO.``Investigation Metadata``.INVESTIGATION.``Investigation Description``.Name) {
-            investigationMetadata
-            |> Validate.ParamCollection.ContainsNonKeyParamWithTerm
-                INVMSO.``Investigation Metadata``.INVESTIGATION.``Investigation Description``
-        }
-        // Investigation has contacts with name, last name, affiliation and email
-        // Investigation Person First Name
-        ARCExpect.validationCase (TestID.Name $"{INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person First Name``.Name} exists") {
-            investigationMetadata
-            |> Validate.ParamCollection.ContainsNonKeyParamWithTerm INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person First Name``
-        }
-        ARCExpect.validationCase (TestID.Name $"{INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person First Name``.Name} is not empty") {
-            investigationMetadata
-            |> Seq.filter (Param.getTerm >> (=) INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person First Name``)
-            |> Seq.iter Validate.Param.ValueIsNotEmpty
-        }
-        // Investigation Person Last Name
-        ARCExpect.validationCase (TestID.Name $"{INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Last Name``.Name} exists") {
-            investigationMetadata
-            |> Validate.ParamCollection.ContainsNonKeyParamWithTerm INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Last Name``
-        }
-        ARCExpect.validationCase (TestID.Name $"{INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Last Name``.Name} is not empty") {
-            investigationMetadata
-            |> Seq.filter (Param.getTerm >> (=) INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Last Name``)
-            |> Seq.iter Validate.Param.ValueIsNotEmpty
-        }
-        // Investigation Person Affiliation
-        ARCExpect.validationCase (TestID.Name $"{INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Affiliation``.Name} exists") {
-            investigationMetadata
-            |> Validate.ParamCollection.ContainsNonKeyParamWithTerm INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Affiliation``
-        }
-        ARCExpect.validationCase (TestID.Name $"{INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Affiliation``.Name} is not empty") {
-            investigationMetadata
-            |> Seq.filter (Param.getTerm >> (=) INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Affiliation``)
-            |> Seq.filter (Param.getValueAsString >> (<>) "Metadata Section Key")
-            |> Seq.iter Validate.Param.ValueIsNotEmpty
-        }
-        // Investigation Person Email
-        ARCExpect.validationCase (TestID.Name $"{INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Email``.Name} exists") {
-        investigationMetadata
-        |> Validate.ParamCollection.ContainsNonKeyParamWithTerm INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Email``
-        }
-        ARCExpect.validationCase (TestID.Name INVMSO.``Investigation Metadata``. ``INVESTIGATION CONTACTS``.``Investigation Person Email``.Name) {
-            investigationMetadata
-            |> Seq.filter (Param.getTerm >> (=) INVMSO.``Investigation Metadata``.``INVESTIGATION CONTACTS``.``Investigation Person Email``)
-            |> Seq.filter (Param.getValueAsString >> (<>) "Metadata Section Key")
-            |> Seq.iter (Validate.Param.ValueMatchesRegex StringValidationPattern.email)
-        }
-    ]
+[<Fact>]
+let ``result Exitcode is 0`` () =
+    let result = runTool "dotnet" [|"fsi"; "../../validation_packages/invenio/invenio@3.0.0.fsx"|] "fixtures/ArcPrototype"
 
-// Execution:
+    Assert.Equal(0, result.ExitCode)
 
-Setup.ValidationPackage(
-    metadata = Setup.Metadata(PACKAGE_METADATA),
-    CriticalValidationCases = [cases]
-)
-|> Execute.ValidationPipeline(
-    basePath = arcDir
-)
+[<Fact>]
+let ``validation_summary.json is equal`` () =
+    let arcExpectValidationResult = ARCExpect.ValidationSummary.fromJson (File.ReadAllText "fixtures/ArcPrototype/.arc-validate-results/invenio@3.0.0/validation_summary.json")
+    Assert.Equal(ReferenceObjects.invenio.validationResultCritical, arcExpectValidationResult.Critical)
+    Assert.Equal(ReferenceObjects.invenio.validationResultNonCritical, arcExpectValidationResult.NonCritical)
