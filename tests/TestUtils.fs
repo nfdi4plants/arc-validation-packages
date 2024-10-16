098cc0015f628bf97e182c1f323d92010978279a
Oliver Maus <oliver.maus@eclipso.de>
2024-10-16 19:14:26 +0200

    Update test utils

--- tests/TestUtils.fs
+++ tests/TestUtils.fs
@@ -1,10 +1,11 @@
 ﻿module TestUtils
 
-open System
-open System.IO
-open type System.Environment
+
+open System.Xml
+
 open Fake.Core
 open Fake.DotNet
+
 
 let runScriptWithArgs (scriptPath: string) (args: string []) =
     let args = Array.concat [|[|scriptPath|]; args|]
@@ -28,3 +29,40 @@ let runTool (tool: string) (args: string []) (dir:string) =
 let getCommitHash path = 
     let output = runTool "git" [|"rev-parse"; "HEAD"|] path
     output
+
+
+type JUnitResults = {
+    PassedTests: string list
+    FailedTests: string list
+    ErroredTests: string list
+} with
+    static member fromJUnitFile (path : string) =
+        let doc = new XmlDocument()
+        doc.Load(path)
+        let suite = doc.SelectNodes("/testsuites/testsuite[@name='arc-validate']").Item(0)
+        let testCases = suite.SelectNodes("testcase") |> Seq.cast<XmlNode>
+        {
+            PassedTests =
+                testCases
+                |> Seq.filter (fun tc -> tc.SelectNodes("failure").Count = 0)
+                |> Seq.map (fun tc -> tc.Attributes.["name"].Value)
+                |> Seq.toList
+                |> List.sort
+
+            FailedTests =
+                testCases
+                |> Seq.filter (fun tc -> tc.SelectNodes("failure").Count > 0)
+                |> Seq.map (fun tc -> tc.Attributes.["name"].Value)
+                |> Seq.toList
+                |> List.sort
+
+            ErroredTests =
+                testCases
+                |> Seq.filter (fun tc -> tc.SelectNodes("error").Count > 0)
+                |> Seq.map (fun tc -> tc.Attributes.["name"].Value)
+                |> Seq.toList
+                |> List.sort
+        }
+
+    static member getTotalTestCount (res : JUnitResults) =
+        res.ErroredTests.Length + res.FailedTests.Length + res.PassedTests.Length
