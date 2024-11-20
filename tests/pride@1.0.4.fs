decd846f89739423f66db682689a3c6a3a659b2c
omaus <omaus@bio.uni-kl.de>
2024-11-20 12:21:36 +0100

    Add unit tests for PRIDE 1.0.4 (WIP)

---
+++ tests/pride@1.0.4.fs
@@ -0,0 +1,88 @@
+﻿module pride_1_0_4
+
+
+open ValidationPackages.Tests
+open TestUtils
+
+open Xunit
+open ARCExpect
+
+open System.IO
+open System
+
+
+type BaseTool_Fixture(scriptName : string, version : string, arcfolder : string) =
+
+    let result = runTool "dotnet" [|"fsi"; $"../../validation_packages/{scriptName}/{scriptName}@{version}.fsx"|] $"fixtures/{arcfolder}"
+
+    let arcExpectValidationResult = ARCExpect.ValidationSummary.fromJson (File.ReadAllText $"fixtures/{arcfolder}/.arc-validate-results/{scriptName}@{version}/validation_summary.json")
+
+    let jUnitResult = JUnitResults.fromJUnitFile $"fixtures/{arcfolder}/.arc-validate-results/{scriptName}@{version}/validation_report.xml"
+
+    let jUnitExpected = JUnitResults.fromJUnitFile $"fixtures/validationReport/{scriptName}/{version}/{arcfolder}/validation_report.xml"
+
+    interface IDisposable with
+        override this.Dispose() =
+            Directory.Delete($"fixtures/{arcfolder}/.arc-validate-results/{scriptName}@{version}/", true)
+
+    member this.Result = result
+
+    member this.ArcExpectValidationResult = arcExpectValidationResult
+
+    member this.JUnitResult = jUnitResult
+
+    member this.JUnitExpected = jUnitExpected
+
+
+type testARC_proteomicsCorrect_Fixture() =
+
+    inherit BaseTool_Fixture("pride", "1.0.4", "testARC_proteomicsCorrect")
+
+
+type testARC_proteomicsCorrect() =
+
+    let tool_fixture = new testARC_proteomicsCorrect_Fixture()
+
+    interface IClassFixture<testARC_proteomicsCorrect_Fixture>
+
+    member this.Fixture with get() = tool_fixture
+
+    [<Fact>]
+    member this.``result Exitcode is 0`` () =
+        Assert.Equal(0, this.Fixture.Result.ExitCode)
+
+    [<Fact>]
+    member this.``validation_summary JSON is equal`` () =
+        Assert.Equal(ReferenceObjects.pride.``1_0_4``.testARC_proteomicsCorrect.validationResultCritical, this.Fixture.ArcExpectValidationResult.Critical)
+        Assert.Equal(ReferenceObjects.pride.``1_0_4``.testARC_proteomicsCorrect.validationResultNonCritical, this.Fixture.ArcExpectValidationResult.NonCritical)
+
+    [<Fact>]
+    member this.``validation_report XML is equal`` () =
+        Assert.Equal(this.Fixture.JUnitExpected, this.Fixture.JUnitResult)
+
+
+type testARC_proteomicsModifValueMissing_Fixture() =
+
+    inherit BaseTool_Fixture("pride", "1.0.4", "testARC_proteomicsCorrect")
+
+
+type testARC_proteomicsModifValueMissing() =
+
+    let tool_fixture = new testARC_proteomicsModifValueMissing_Fixture()
+
+    interface IClassFixture<testARC_proteomicsModifValueMissing_Fixture>
+
+    member this.Fixture with get() = tool_fixture
+
+    [<Fact>]
+    member this.``result Exitcode is 0`` () =
+        Assert.Equal(0, this.Fixture.Result.ExitCode)
+
+    [<Fact>]
+    member this.``validation_summary JSON is equal`` () =
+        Assert.Equal(ReferenceObjects.pride.``1_0_4``.testARC_proteomicsCorrect.validationResultCritical, this.Fixture.ArcExpectValidationResult.Critical)
+        Assert.Equal(ReferenceObjects.pride.``1_0_4``.testARC_proteomicsCorrect.validationResultNonCritical, this.Fixture.ArcExpectValidationResult.NonCritical)
+
+    [<Fact>]
+    member this.``validation_report XML is equal`` () =
+        Assert.Equal(this.Fixture.JUnitExpected, this.Fixture.JUnitResult)
